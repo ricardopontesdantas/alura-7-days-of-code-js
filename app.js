98fa89b54b941d0c4cf3d22d4329ce41e32fdc03
ricardopontesdantas <ricardopontesdantas@gmail.com>
2022-04-16 11:32:43 -0300

    Add form validation

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const form = document.querySelector('[data-js="form"]')
 
+const name = document.querySelector('[data-js="name"]')
+const birth = document.querySelector('[data-js="birth"]')
+
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    
+
     const { name, birth } = form
     console.log({ name: name.value, birth: birth.value })
 })
+
+name.oninvalid = function () {
+    this.setCustomValidity('')
+
+    if (!this.validity.valid) {
+        this.setCustomValidity('O nome deve conter apenas letras (no mínimo 3 e no máximo 150)')
+    }
+}
+
+birth.oninvalid = function () {
+    this.setCustomValidity('')
+
+    if (!this.validity.valid) {
+        this.setCustomValidity('O formato da data deve ser DD/MM/AAAA e os meses devem estar entre 01 e 12')
+    }
+}
